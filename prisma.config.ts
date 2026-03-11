7b5b025b35a590fdd97d1ecc81e734f0d0b2611e
NathanvanderVeren <nathanvanderveren@gmail.com>
2026-03-11 16:13:59 +0100

    Auto-save: 2026-03-11 16:13:59

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,22 +1,14 @@
 import { defineConfig } from "prisma/config";
-import pg from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 export default defineConfig({
   datasource: {
-    adapter: () => {
-      const pool = new pg.Pool({
-        connectionString: process.env.DATABASE_URL,
-      });
-      return new PrismaPg(pool);
-    },
+    adapter: () => new PrismaPg({ connectionString: process.env.DATABASE_URL }),
   },
   migrate: {
-    adapter: () => {
-      const pool = new pg.Pool({
+    adapter: () =>
+      new PrismaPg({
         connectionString: process.env.DIRECT_URL ?? process.env.DATABASE_URL,
-      });
-      return new PrismaPg(pool);
-    },
+      }),
   },
 });

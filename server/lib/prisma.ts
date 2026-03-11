7ccf9f5e302d0b040e35fc725c75470331930d29
NathanvanderVeren <nathanvanderveren@gmail.com>
2026-03-11 16:13:54 +0100

    Auto-save: 2026-03-11 16:13:54

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,10 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
 
 function createPrismaClient() {
-  const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
-  const adapter = new PrismaPg(pool);
+  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
   return new PrismaClient({ adapter });
 }
 
